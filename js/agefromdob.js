68f5cae68204aace476de1b533affbed588079f2
TheEarThPr <panot1997@hotmail.com>
2016-11-13 01:54:21 +0700

    Edit

--- js/agefromdob.js
+++ js/agefromdob.js
@@ -1,5 +1,17 @@
-function calculateAge(dob) { // birthday is a date
-    var ageDifMs = Date.now() - dob.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
+$(function calculateAge() {
+        $('dob').datepicker({ 
+          format: "yyyy-mm-dd",
+          autoclose: true, 
+          onSelect:function(dateText){
+            var today = new Date();
+            var birthDate = new Date(dateText);
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+            {
+                age--;
+            }
+            $('age').text(age);
+           }
+       });
+    });
