86453fcab85134df76a4a6091365ed8c8cca770c
TheEarThPr <panot1997@hotmail.com>
2016-11-12 17:27:22 +0700

    Edit js

--- js/calcories.js
+++
@@ -1,6 +0,0 @@
-$(document).on('click','#menuplus', function(){
-  $('#counter').text(Number($('#counter').text())+1);
-});
-$(document).on('click','#menuminus', function(){
-  $('#counter').text(Number($('#counter').text())-1);
-});
