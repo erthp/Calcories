2a40c321844146941a87b98939841312f7d9b0e3
TheEarThPr <panot1997@hotmail.com>
2016-11-13 01:27:10 +0700

    Edit var

--- js/agefromdob.js
+++ js/agefromdob.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 function ageCount() {
     var date1 = new Date();
-    var dob = document.getElementById("dob").value;
+    var dob = document.getElementById("from-date").value;
     var date2 = new Date(dob);
     var pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
     //Regex to validate date format (dd/mm/yyyy)       
@@ -12,11 +12,11 @@ function ageCount() {
         //getting dob year            
         var age = y1 - y2;
         //calculating age                       
-        document.getElementById("ageId").value = age;
-        doucment.getElementById("ageId").focus ();
+        document.getElementById("from-age").value = age;
+        doucment.getElementById("from-age").focus ();
         return true;
     } else {
-        alert("Invalid date format. Please Input in (dd/mm/yyyy) format!");
+        alert("Invalid date format. Please Input in (dd/mm/yyyy) format.");
         return false;
     }
 
