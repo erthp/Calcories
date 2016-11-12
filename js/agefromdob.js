0c381a39dbeb6601ba150595b02253f93b141c8a
TheEarThPr <panot1997@hotmail.com>
2016-11-13 02:05:56 +0700

    Edit var

--- js/agefromdob.js
+++ js/agefromdob.js
@@ -1,17 +1,29 @@
-$(function calculateAge() {
-        $('dob').datepicker({ 
-          format: "yyyy-mm-dd",
-          autoclose: true, 
-          onSelect:function(dateText){
-            var today = new Date();
-            var birthDate = new Date(dateText);
-            var age = today.getFullYear() - birthDate.getFullYear();
-            var m = today.getMonth() - birthDate.getMonth();
-            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-            {
-                age--;
-            }
-            $('age').text(age);
-           }
-       });
-    });
+var curDate = new Date();
+
+    var curr_year = curDate.getFullYear();
+    var curr_month = curDate.getMonth();
+
+
+    //Calculates age from given Birth Date in the form//
+    function calcAge() {
+
+        var dt1 = document.getElementById('dob').value;
+        var birth_date = new Date(dt1);
+
+        var birth_year = birth_date.getFullYear();
+        var birth_month = birth_date.getMonth();
+        var calc_year = curr_year - birth_year;
+        var calc_month = curr_month - birth_month;
+
+        //The following below is what I am not sure about. 
+        //I need to combine years and months and
+        //convert them into a string??? Is this syntax dead wrong?
+
+    var final_result = (calc_year && "." && calc_month).toString();
+
+        // final result should be a number with a decimal point, example: 35.5
+        final_result = parseFloat;
+
+        return (final_result);
+        alert(final_result);
+    }
